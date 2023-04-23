21fbeef5a7afb3c5a1809c1555c8ff6c5ab02647
jsergio <jsergiosette@gmail.com>
2023-04-23 20:09:43 +0000

    agora com 404.html

--- src/app/model/aux.ts
+++ src/app/model/aux.ts
@@ -7,7 +7,7 @@ export interface Info {
   }
     // const pp:Pilha = new Pilha()
     
-    export const pp:Info[] = [
+    export const pp1:Info[] = [
     {id: 40, direcao: 'esq'},
     {id: 41, direcao: 'baixo'},
     {id: 23, direcao: 'baixo'},
@@ -50,4 +50,49 @@ export interface Info {
     {id: 57, direcao: 'cima'},
     {id: 75, direcao: 'esq'}
   ]
+
+  export const pp:Info[]=[
+    {id: 40, direcao: 'esq'},
+    {id: 41, direcao: 'baixo'},
+    {id: 23, direcao: 'baixo'},
+    {id: 5, direcao: 'dir'},
+    {id: 32, direcao: 'cima'},
+    {id: 14, direcao: 'cima'},
+    {id: 23, direcao: 'baixo'},
+    {id: 14, direcao: 'dir'},
+    {id: 32, direcao: 'baixo'},
+    {id: 23, direcao: 'dir'},
+    {id: 41, direcao: 'baixo'},
+    {id: 32, direcao: 'esq'},
+    {id: 34, direcao: 'cima'},
+    {id: 33, direcao: 'esq'},
+    {id: 35, direcao: 'cima'},
+    {id: 34, direcao: 'dir'},
+    {id: 33, direcao: 'esq'},
+    {id: 32, direcao: 'dir'},
+    {id: 31, direcao: 'esq'},
+    {id: 42, direcao: 'dir'},
+    {id: 33, direcao: 'cima'},
+    {id: 30, direcao: 'dir'},
+    {id: 32, direcao: 'dir'},
+    {id: 31, direcao: 'esq'},
+    {id: 40, direcao: 'cima'},
+    {id: 49, direcao: 'baixo'},
+    {id: 28, direcao: 'cima'},
+    {id: 29, direcao: 'dir'},
+    {id: 27, direcao: 'cima'},
+    {id: 30, direcao: 'cima'},
+    {id: 28, direcao: 'esq'},
+    {id: 29, direcao: 'dir'},
+    {id: 48, direcao: 'cima'},
+    {id: 46, direcao: 'esq'},
+    {id: 47, direcao: 'baixo'},
+    {id: 48, direcao: 'dir'},
+    {id: 50, direcao: 'dir'},
+    {id: 41, direcao: 'cima'},
+    {id: 66, direcao: 'esq'},
+    {id: 57, direcao: 'cima'},
+    {id: 75, direcao: 'esq'}
+  ]
+  
   
